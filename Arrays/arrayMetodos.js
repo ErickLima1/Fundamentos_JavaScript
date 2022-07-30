9051eebc833b804519bdce32b341dbe7f2693000
ErickLima <ericklimacosta29@gmail.com>
2022-07-30 14:04:59 -0300

    Operação Array

--- Arrays/arrayMetodos.js
+++ Arrays/arrayMetodos.js
@@ -24,7 +24,7 @@ console.log(pilotos);
 const algunsPilotos1 = pilotos.slice(2); //Novo array gerado por esse metodo
 console.log(algunsPilotos1);
 
-const algunsPilotos2 = pilotos.slice(1, 4);
+const algunsPilotos2 = pilotos.slice(1, 5);
 console.log(algunsPilotos2);
 
 
